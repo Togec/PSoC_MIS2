4281198836d09db1537c624017158840eef6a097
Togec <40124015+Togec@users.noreply.github.com>
2018-07-28 18:08:22 +0300

    Atskirtas projektas
    180604 versija. Ismestas MIS_WIRE

--- stendo_MIS_WIRE.cydsn/SPI.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SPI_H__
-#define __SPI_H__    
-
-     void SPI_main(uint32 TotCounter);
-    
-    
- #endif
--- stendo_MIS_WIRE.cydsn/assert.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ASSERT_H__
-#define __ASSERT_H__
-
-extern char text[500];
-extern void __assert__(int test, char msg[], char file[], int line);
-
-#define assert(test, msg) __assert__(test, msg,  __FILE__, __LINE__)
-
-#endif
